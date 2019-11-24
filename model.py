511810a2e033416885fce9f419748006f87ffde4
Rodrigo Araújo Marinho Franco <contact@rodrigoamf.com>
2019-11-24 19:43:18 -0300

    #18 Add early stopping and improve model

--- model.py
+++ model.py
@@ -18,11 +18,11 @@ class Net(nn.Module):
         
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        self.fc1 = torch.nn.Linear(4096, 512)
-        self.fc2 = torch.nn.Linear(512, 512)
+        self.fc1 = torch.nn.Linear(16384, 2048)
+        self.fc2 = torch.nn.Linear(2048, 512)
         self.fc3 = torch.nn.Linear(512, 7)
         
-        self.dropout50 = nn.Dropout(0.50)
+        self.dropout20 = nn.Dropout(0.20)
         
         I.xavier_normal_(self.conv1.weight, gain=1)
         I.xavier_normal_(self.conv2.weight, gain=1)
@@ -34,44 +34,44 @@ class Net(nn.Module):
 
         
     def forward(self, x):
-        # Size changes from (3, 32, 32) to (32, 32, 32)
+        # Size changes from (3, 64, 64) to (32, 64, 64)
         #print(x.shape)
         x = F.leaky_relu(self.conv1(x))
         #print(x.shape)
 
-        # Size changes from (32, 32, 32) to (32, 32, 32)
+        # Size changes from (32, 64, 64) to (32, 64, 64)
         x = F.leaky_relu(self.conv2(x))
         #print(x.shape)
 
-        # Size changes from (32, 32, 32) to (32, 16, 16)
+        # Size changes from (32, 64, 64) to (32, 32, 32)
         x = self.pool(x)
         #print(x.shape)
 
-        # Size changes from (32, 16, 16) to (64, 16, 16)
+        # Size changes from (32, 32, 32) to (64, 32, 32)
         x = F.leaky_relu(self.conv3(x))
         #print(x.shape)
         
-        # Size changes from (64, 16, 16) to (64, 16, 16)
+        # Size changes from (64, 32, 32) to (64, 32, 32)
         x = F.leaky_relu(self.conv4(x))
         #print(x.shape)
 
-        # Size changes from (64, 16, 16) to (64, 8, 8)    
+        # Size changes from (64, 32, 32) to (64, 16, 16)    
         x = self.pool(x)
         #print(x.shape)
 
-        # Size changes from (64, 8, 8) to (4096)
+        # Size changes from (64, 16, 16) to (16384)
         x = x.view(x.size(0), -1)
         
-        # Size changes from (4096) to (512)
+        # Size changes from (16384) to (2048)
         x = F.relu(self.fc1(x))
         #print(x.shape)
-        x = self.dropout50(x)
+        x = self.dropout20(x)
         
-        # Size changes from (512) to (512)
+        # Size changes from (2048) to (512)
         x = F.relu(self.fc2(x))
-        x = self.dropout50(x)
+        x = self.dropout20(x)
         
-        # Size changes from (512) to (6)
+        # Size changes from (512) to (7)
         x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
