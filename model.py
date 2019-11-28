fe7f90cd7ea488c7bb32db831c00174636420aff
Rodrigo Araújo Marinho Franco <contact@rodrigoamf.com>
2019-11-27 22:38:13 -0300

    #22 Notebook to predict image

--- model.py
+++ model.py
@@ -10,68 +10,64 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1, stride=1)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1, stride=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1, stride=1)
+        torch.nn.init.xavier_uniform(self.conv1.weight)
+        self.conv1.bias.data.fill_(0.01)
+        
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1)
+        torch.nn.init.xavier_uniform(self.conv2.weight)
+        self.conv2.bias.data.fill_(0.01)
+        
+        '''self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1, stride=1)
+        torch.nn.init.xavier_uniform(self.conv3.weight)
+        self.conv3.bias.data.fill_(0.01)'''
+        
+        '''self.conv4 = nn.Conv2d(64, 128, kernel_size=3, padding=1, stride=1)
+        torch.nn.init.xavier_uniform(self.conv4.weight)
+        self.conv4.bias.data.fill_(0.01)'''
         
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        self.fc1 = torch.nn.Linear(16384, 2048)
-        self.fc2 = torch.nn.Linear(2048, 512)
+        self.fc1 = torch.nn.Linear(16384, 2098)
+        self.fc2 = torch.nn.Linear(2098, 512)
         self.fc3 = torch.nn.Linear(512, 7)
         
-        self.dropout20 = nn.Dropout(0.20)
+        self.dropout50 = nn.Dropout(0.50)
         
-        I.xavier_normal_(self.conv1.weight, gain=1)
-        I.xavier_normal_(self.conv2.weight, gain=1)
-        I.xavier_normal_(self.conv3.weight, gain=1)
-        I.xavier_normal_(self.conv4.weight, gain=1)
-        I.xavier_normal_(self.fc1.weight, gain=1)
-        I.xavier_normal_(self.fc2.weight, gain=1)
-        I.xavier_normal_(self.fc3.weight, gain=1)
+        #I.xavier_normal_(self.conv1.weight, gain=1)
+        #I.xavier_normal_(self.conv2.weight, gain=1)
+        #I.xavier_normal_(self.conv3.weight, gain=1)
+        #I.xavier_normal_(self.conv4.weight, gain=1)
+        #I.xavier_normal_(self.fc1.weight, gain=1)
+        #I.xavier_normal_(self.fc2.weight, gain=1)
+        #I.xavier_normal_(self.fc3.weight, gain=1)
 
         
     def forward(self, x):
-        # Size changes from (3, 64, 64) to (32, 64, 64)
-        #print(x.shape)
         x = F.leaky_relu(self.conv1(x))
-        #print(x.shape)
-
-        # Size changes from (32, 64, 64) to (32, 64, 64)
-        x = F.leaky_relu(self.conv2(x))
-        #print(x.shape)
-
-        # Size changes from (32, 64, 64) to (32, 32, 32)
-        x = self.pool(x)
-        #print(x.shape)
-
-        # Size changes from (32, 32, 32) to (64, 32, 32)
-        x = F.leaky_relu(self.conv3(x))
-        #print(x.shape)
         
-        # Size changes from (64, 32, 32) to (64, 32, 32)
-        x = F.leaky_relu(self.conv4(x))
-        #print(x.shape)
-
-        # Size changes from (64, 32, 32) to (64, 16, 16)    
         x = self.pool(x)
-        #print(x.shape)
 
-        # Size changes from (64, 16, 16) to (16384)
+        x = F.leaky_relu(self.conv2(x))
+
+        x = self.pool(x)
+
+        '''x = F.leaky_relu(self.conv3(x))
+        
+        x = self.pool(x)
+        
+        x = F.leaky_relu(self.conv4(x))
+
+        x = self.pool(x)'''
+
         x = x.view(x.size(0), -1)
         
-        # Size changes from (16384) to (2048)
         x = F.relu(self.fc1(x))
-        #print(x.shape)
-        x = self.dropout20(x)
+        x = self.dropout50(x)
         
-        # Size changes from (2048) to (512)
         x = F.relu(self.fc2(x))
-        x = self.dropout20(x)
+        x = self.dropout50(x)
         
-        # Size changes from (512) to (7)
         x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
